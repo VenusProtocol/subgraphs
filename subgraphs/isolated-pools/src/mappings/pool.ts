252a293c153bb426cbb951dca5d4c88c82ae948e
Corey Rice <coreyarice@gmail.com>
2022-08-02 18:29:42 -0300

    Add skeleton of isolated pools

---
+++ subgraphs/isolated-pools/src/mappings/pool.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+/* renable when implementing*/
+export const handleMarketListed = () => {};
+
+export const handleMarketEntered = () => {};
+
+export const handleMarketExited = () => {};
+
+export const handleNewCloseFactor = () => {};
+
+export const handleNewCollateralFactor = () => {};
+
+export const handleNewLiquidationIncentive = () => {};
+
+export const handleNewPriceOracle = () => {};
+
+export const handleNewPauseGuardian = () => {};
+
+export const handleGlobalActionPaused = () => {};
+
+export const handleMarketActionPaused = () => {};
+
+export const handleNewBorrowCap = () => {};
+
+export const handleNewBorrowCapGuardian = () => {};
+
+export const handleNewMinLiquidatableAmount = () => {};
---
+++ subgraphs/isolated-pools/src/mappings/poolRegistry.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+/* renable when implementing*/
+import { PoolNameSet, PoolRegistered } from '../../generated/PoolRegistery/PoolRegistery';
+
+export const handlePoolRegistered = (event: PoolRegistered): void => {};
+
+export const handlePoolNameSet = (event: PoolNameSet): void => {};
