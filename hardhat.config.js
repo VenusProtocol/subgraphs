be5b04215eed234ea9ec9474f7a4a5019d1ab026
Corey Rice <coreyarice@gmail.com>
2022-08-19 21:27:25 -0300

    Update deploying local graph docs and commands

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 97,
+    },
+  },
+};
