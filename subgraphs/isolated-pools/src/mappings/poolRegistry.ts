7c1dbb73cf418805a8b7354cf0e0a92bd9e7541a
Corey Rice <coreyarice@gmail.com>
2022-08-24 12:37:26 -0300

    fix: create pool to listen to events

--- subgraphs/isolated-pools/src/mappings/poolRegistry.ts
+++ subgraphs/isolated-pools/src/mappings/poolRegistry.ts
@@ -4,6 +4,7 @@ import {
   PoolRegistry as PoolRegistryContract,
 } from '../../generated/PoolRegistry/PoolRegistry';
 import { Pool } from '../../generated/schema';
+import { Pool as PoolContract } from '../../generated/templates';
 import { poolRegistryAddress } from '../constants/addresses';
 import { createPool } from '../operations/create';
 
@@ -16,6 +17,7 @@ export const handlePoolNameSet = (event: PoolNameSet): void => {
   const poolIndex = event.params.index;
   const poolRegistryContract = PoolRegistryContract.bind(poolRegistryAddress);
   const poolData = poolRegistryContract.getPoolByID(poolIndex);
+  PoolContract.create(poolData.comptroller);
   const pool = Pool.load(poolData.comptroller.toHexString());
   if (pool) {
     pool.name = event.params.name;
