e35021aa1bc33d0affbb352f540605595ecb4848
defistar <70688600+defistar@users.noreply.github.com>
2022-08-25 18:40:30 +0530

    isolated-pools fix template.yaml to gave ABI entry of PoolLens

--- subgraphs/isolated-pools/src/constants/addresses.ts
+++ subgraphs/isolated-pools/src/constants/addresses.ts
@@ -3,3 +3,5 @@ import { Address } from '@graphprotocol/graph-ts';
 export const poolRegistryAddress = Address.fromString('0x0000000000000000000000000000000000000000');
 
 export const vBnbAddress = Address.fromString('0xa07c5b74c9b40447a954e1466938b865b6bbea36');
+
+export const poolLensAddress = Address.fromString('0x0000000000000000000000000000000000000000');
