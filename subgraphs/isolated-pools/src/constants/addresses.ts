2e189a605c9c0676e88891943aca3e8eb1f4a3e2
Corey Rice <coreyarice@gmail.com>
2022-08-31 17:45:40 -0300

    Give pool lens a unique address

--- subgraphs/isolated-pools/src/constants/addresses.ts
+++ subgraphs/isolated-pools/src/constants/addresses.ts
@@ -4,4 +4,4 @@ export const poolRegistryAddress = Address.fromString('0x00000000000000000000000
 
 export const vBnbAddress = Address.fromString('0xa07c5b74c9b40447a954e1466938b865b6bbea36');
 
-export const poolLensAddress = Address.fromString('0x0000000000000000000000000000000000000000');
+export const poolLensAddress = Address.fromString('0xa1a000000000000000000000000000000000000');
