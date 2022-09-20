eebcc7657ff2564284b056fddb0ef1a939bd9544
Corey Rice <coreyarice@gmail.com>
2022-09-20 10:50:39 -0500

    Fix isolated pools import

--- subgraphs/isolated-pools/src/utilities/index.ts
+++ subgraphs/isolated-pools/src/utilities/index.ts
@@ -4,3 +4,4 @@ export { default as getReserveFactorMantissa } from './getReserveFactorMantissa'
 export { default as getUnderlyingAddress } from './getUnderlyingAddress';
 export { default as getBnbPriceInUsd } from './getBnBPriceInUsd';
 export { default as getTokenPriceInUsd } from './getTokenPriceInUsd';
+export { default as exponentToBigDecimal } from './exponentToBigDecimal';
