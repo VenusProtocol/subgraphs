3f497d5e660121f06a3eceb4af245de7482e8786
Corey Rice <coreyarice@gmail.com>
2022-09-28 09:04:25 -0500

    copy contracts

---
+++ copy_contracts.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+# For convience we are going to copy contracts and artifacts locally so they can be 
+
+rm -rf ./contracts
+mkdir -p ./contracts/oracle
+mkdir -p ./contracts/isolated-pools
+cp -rf ./node_modules/@venusprotocol/oracle/contracts/ ./contracts/oracle/contracts
+cp -rf ./node_modules/@venusprotocol/isolated-pools/contracts/ ./contracts/isolated-pools/contracts
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,43 +4,41 @@ import 'hardhat-deploy';
 import { HardhatUserConfig } from 'hardhat/config';
 import 'module-alias/register';
 
-const packageCompilerVersions = {
-  'venus-governance': '0.5.16',
-  'isolated-pools': {
-    compilers: [
-      {
-        version: '0.8.13',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-          outputSelection: {
-            '*': {
-              '*': ['storageLayout'],
-            },
+const compilers = {
+  compilers: [
+    { version: '0.5.16' },
+    {
+      version: '0.8.13',
+      settings: {
+        optimizer: {
+          enabled: true,
+        },
+        outputSelection: {
+          '*': {
+            '*': ['storageLayout'],
           },
         },
       },
-      {
-        version: '0.6.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-          outputSelection: {
-            '*': {
-              '*': ['storageLayout'],
-            },
+    },
+    {
+      version: '0.6.6',
+      settings: {
+        optimizer: {
+          enabled: true,
+        },
+        outputSelection: {
+          '*': {
+            '*': ['storageLayout'],
           },
         },
       },
-    ],
-  },
-};
+    },
+  ],
+}
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
-  solidity: packageCompilerVersions[process.env.PACKAGE as keyof typeof packageCompilerVersions],
+  solidity: compilers,
   networks: {
     hardhat: {
       chainId: 56,
@@ -48,25 +46,15 @@ const config: HardhatUserConfig = {
     },
   },
   paths: {
-    sources: `${__dirname}/subgraphs/${process.env.PACKAGE}/contracts`,
-    artifacts: `${__dirname}/subgraphs/${process.env.PACKAGE}/artifacts`,
+    sources: `${__dirname}/contracts`,
+    artifacts: `${__dirname}/artifacts`,
   },
   // Hardhat deploy
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
     },
-  },
-  external: {
-    contracts: [
-      {
-        artifacts: `${__dirname}/node_modules/@venusprotocol/isolated-pools/artifacts/`,
-      },
-      {
-        artifacts: `${__dirname}/node_modules/@venusprotocol/oracle/artifacts/`,
-      },
-    ],
-  },
+  }
 };
 
 export default config;
