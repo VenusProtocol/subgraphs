651a1e21b21427ab1108b42e47241e15c92ae2db
Corey Rice <coreyarice@gmail.com>
2022-10-21 07:23:21 -0300

    bump typescript version

--- subgraphs/isolated-pools/jest.config.ts
+++ subgraphs/isolated-pools/jest.config.ts
@@ -153,7 +153,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/integration/?(*.)+(spec|test).[tj]s?(x)'],
+  testMatch: ['**/integration/?(*.)+(spec).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
