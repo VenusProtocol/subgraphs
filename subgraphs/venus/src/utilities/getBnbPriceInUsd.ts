d499098c9d33446481060fb75da1d36a981f6e66
Corey Rice <coreyarice@gmail.com>
2023-09-04 12:14:38 -0300

    refactor: use abis from venus-protocol package

--- subgraphs/venus/src/utilities/getBnbPriceInUsd.ts
+++ subgraphs/venus/src/utilities/getBnbPriceInUsd.ts
@@ -1,6 +1,6 @@
 import { Address, BigDecimal } from '@graphprotocol/graph-ts';
 
-import { PriceOracle2 } from '../../generated/templates/VToken/PriceOracle2';
+import { PriceOracle } from '../../generated/templates/VToken/PriceOracle';
 import { vBnbAddress } from '../constants/addresses';
 import { getOrCreateComptroller } from '../operations/getOrCreate';
 import { mantissaFactorBD } from './exponentToBigDecimal';
@@ -9,7 +9,7 @@ export function getBnbPriceInUsd(): BigDecimal {
   const comptroller = getOrCreateComptroller();
 
   const oracleAddress = Address.fromBytes(comptroller.priceOracle);
-  const oracle = PriceOracle2.bind(oracleAddress);
+  const oracle = PriceOracle.bind(oracleAddress);
   const bnbPriceInUSD = oracle.getUnderlyingPrice(vBnbAddress).toBigDecimal().div(mantissaFactorBD);
   return bnbPriceInUSD;
 }
