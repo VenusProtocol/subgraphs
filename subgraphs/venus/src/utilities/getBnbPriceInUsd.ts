bf53351edbc8c954aee59bc6f62111a5a4c29bf9
Corey Rice <coreyarice@gmail.com>
2023-09-20 14:03:28 -0300

    chore: :shirt:

--- subgraphs/venus/src/utilities/getBnbPriceInUsd.ts
+++ subgraphs/venus/src/utilities/getBnbPriceInUsd.ts
@@ -1,15 +1,18 @@
 import { Address, BigDecimal } from '@graphprotocol/graph-ts';
 
 import { PriceOracle } from '../../generated/templates/VToken/PriceOracle';
+import { mantissaFactorBigDecimal } from '../constants';
 import { vBnbAddress } from '../constants/addresses';
 import { getOrCreateComptroller } from '../operations/getOrCreate';
-import { mantissaFactorBigDecimal } from '../constants';
 
 export function getBnbPriceInUsd(): BigDecimal {
   const comptroller = getOrCreateComptroller();
 
   const oracleAddress = Address.fromBytes(comptroller.priceOracle);
-const oracle = PriceOracle.bind(oracleAddress);
-  const bnbPriceInUSD = oracle.getUnderlyingPrice(vBnbAddress).toBigDecimal().div(mantissaFactorBigDecimal);
+  const oracle = PriceOracle.bind(oracleAddress);
+  const bnbPriceInUSD = oracle
+    .getUnderlyingPrice(vBnbAddress)
+    .toBigDecimal()
+    .div(mantissaFactorBigDecimal);
   return bnbPriceInUSD;
 }
