7f40eb29a023cd0a16dfd09383cf0ac2c91117b0
Corey Rice <coreyarice@gmail.com>
2023-10-11 13:47:14 -0300

    fix: increase timeout

--- subgraphs/venus-governance/tests/integration/setup.ts
+++ subgraphs/venus-governance/tests/integration/setup.ts
@@ -4,6 +4,7 @@ import { SUBGRAPH_ACCOUNT, SUBGRAPH_NAME, SYNC_DELAY } from './utils/constants';
 
 describe('Deploy Subgraph', function () {
   it('should deploy subgraph', async function () {
+    this.timeout(600000); // sometimes it takes a long time
     const root = `${__dirname}/../..`;
 
     await deploy({
