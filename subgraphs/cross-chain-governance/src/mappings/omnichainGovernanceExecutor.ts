e0dfeef23a85247380216b147af4ca90ed80e18b
Corey Rice <coreyarice@gmail.com>
2023-10-31 13:33:10 -0300

    feat: sketch out cross chain governance subgraphs

---
+++ subgraphs/cross-chain-governance/src/mappings/omnichainGovernanceExecutor.ts
@@ -0,0 +1,25 @@
+export function handleSetMaxDailyReceiveLimit() {
+
+}
+
+export function handleProposalReceived() {
+
+}
+export function handleProposalQueued() {
+
+}
+export function handleProposalExecuted() {
+
+}
+export function handleProposalFailed() {
+
+}
+export function handleProposalCancelled() {
+
+}
+export function handleSetExecuteDelay() {
+
+}
+export function handleTimelockAdded() {
+
+}
