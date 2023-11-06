169d2045377ded90149d46415c2918b59d09c2e1
Corey Rice <coreyarice@gmail.com>
2023-11-06 13:19:20 -0300

    feat: add deploy scripts ;

---
+++ deploy/020-omnichain-local.ts
@@ -0,0 +1,5 @@
+import omnichainLocal from '@venusprotocol/cross-chain-governance-contracts/dist/deploy/002-omnichain-local.ts';
+
+omnichainLocal.skip = () => false;
+
+export default omnichainLocal;
---
+++ deploy/021-omnichaing-remote.ts
@@ -0,0 +1,5 @@
+import omnichainRemote from '@venusprotocol/cross-chain-governance-contracts/dist/deploy/003-omnichain-remote.ts';
+
+omnichainRemote.skip = () => false;
+
+export default omnichainRemote;
