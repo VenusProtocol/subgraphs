1a52e09157dc8909c1d4823b6da744d8c47c2897
Corey Rice <coreyarice@gmail.com>
2023-11-30 16:43:46 -0300

    feat: add cross-chain deployment for integration tests

--- deploy/021-omnichaing-remote.ts
+++
@@ -1,5 +0,0 @@
-import omnichainRemote from '@venusprotocol/cross-chain-governance-contracts/dist/deploy/003-omnichain-remote.ts';
-
-omnichainRemote.skip = () => false;
-
-export default omnichainRemote;
