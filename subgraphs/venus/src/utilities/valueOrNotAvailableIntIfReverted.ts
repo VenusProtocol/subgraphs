c8e0b0d877650435d2be65fe3520906f96d0d69b
Corey Rice <coreyarice@gmail.com>
2024-01-29 16:12:31 -0300

    refactor: remove duplicate comment

--- subgraphs/venus/src/utilities/valueOrNotAvailableIntIfReverted.ts
+++ subgraphs/venus/src/utilities/valueOrNotAvailableIntIfReverted.ts
@@ -3,7 +3,6 @@ import { BigInt, ethereum, log } from '@graphprotocol/graph-ts';
 import { NOT_AVAILABLE_BIG_INT } from '../constants';
 
 // checks if a call reverted, in case it is we return -1 to indicate the wanted value is not available
-// checks if a call reverted, in case it is we return -1 to indicate the wanted value is not available
 export function valueOrNotAvailableIntIfReverted(
   callResult: ethereum.CallResult<BigInt>,
   callName: string,
