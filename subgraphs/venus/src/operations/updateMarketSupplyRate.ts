1ec26276bca7de6f8bba64ddb0858600458bcd47
Gleiser Oliveira <gleiser.dev@gmail.com>
2024-02-23 07:48:30 -0600

    feat: add updateMarketSupplyRate operation

---
+++ subgraphs/venus/src/operations/updateMarketSupplyRate.ts
@@ -0,0 +1,12 @@
+import { Market } from '../../generated/schema';
+import { VToken } from '../../generated/templates/VToken/VToken';
+import { valueOrNotAvailableIntIfReverted } from '../utilities';
+
+export function updateMarketSupplyRate(market: Market, vTokenContract: VToken): void {
+  // This fails on only the first call to cZRX. It is unclear why, but otherwise it works.
+  // So we handle it like this.
+  market.supplyRateMantissa = valueOrNotAvailableIntIfReverted(
+    vTokenContract.try_supplyRatePerBlock(),
+    'vBEP20 try_supplyRatePerBlock()',
+  );
+}
