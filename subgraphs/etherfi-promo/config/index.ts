e0aad232df6f816e9df763ac224ba6a01f722808
Corey Rice <coreyarice@gmail.com>
2024-09-20 08:28:06 -0300

    feat: add etherfi promo subgraph for bnb

--- subgraphs/etherfi-promo/config/index.ts
+++ subgraphs/etherfi-promo/config/index.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 import Mustache from 'mustache';
 
 export const getNetwork = () => {
-  const supportedNetworks = ['sepolia', 'ethereum', 'docker'] as const;
+  const supportedNetworks = ['sepolia', 'ethereum', 'docker', 'bsc'] as const;
   const network = process.env.NETWORK as (typeof supportedNetworks)[number];
   if (!supportedNetworks.includes(network)) {
     throw new Error(`NETWORK env var must be set to one of ${supportedNetworks}`);
@@ -22,6 +22,7 @@ const main = () => {
       vWeEthsAddress: sepoliaILDeployments.addresses.VToken_vweETHs_LiquidStakedETH,
       vWeEthStartBlock: '0',
       vWeEthsStartBlock: '0',
+      template: 'template.yaml',
     },
     sepolia: {
       network: 'sepolia',
@@ -29,6 +30,7 @@ const main = () => {
       vWeEthsAddress: sepoliaILDeployments.addresses.VToken_vweETHs_LiquidStakedETH,
       vWeEthStartBlock: '5659827',
       vWeEthsStartBlock: '6536644',
+      template: 'template.yaml',
     },
     ethereum: {
       network: 'mainnet',
@@ -36,10 +38,16 @@ const main = () => {
       vWeEthsAddress: ethereumILDeployments.addresses.VToken_vweETHs_LiquidStakedETH,
       vWeEthStartBlock: '19638180',
       vWeEthsStartBlock: '20583508',
+      template: 'template.yaml',
+    },
+    bsc: {
+      network: 'bsc',
+      vWeEthAddress: '0xc5b24f347254bD8cF8988913d1fd0F795274900F',
+      vWeEthStartBlock: '41956130',
+      template: 'template-bsc.yaml',
     },
   };
-
-  const yamlTemplate = fs.readFileSync('template.yaml', 'utf8');
+  const yamlTemplate = fs.readFileSync(config[network].template, 'utf8');
   const yamlOutput = Mustache.render(yamlTemplate, config[network]);
   fs.writeFileSync('subgraph.yaml', yamlOutput);
 };
