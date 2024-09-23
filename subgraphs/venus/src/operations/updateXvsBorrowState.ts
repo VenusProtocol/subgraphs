529b68ef8a1fc083a6e33731b9a61a3f8db33b1f
Corey Rice <coreyarice@gmail.com>
2024-09-23 13:49:28 -0300

    refactor: remove extra comptroller calls

--- subgraphs/venus/src/operations/updateXvsBorrowState.ts
+++ subgraphs/venus/src/operations/updateXvsBorrowState.ts
@@ -1,12 +1,10 @@
 import { Address } from '@graphprotocol/graph-ts';
 
 import { Comptroller } from '../../generated/Comptroller/Comptroller';
-import { VToken } from '../../generated/Comptroller/VToken';
 import { Market } from '../../generated/schema';
+import { comptrollerAddress } from '../constants/addresses';
 
 export function updateXvsBorrowState(market: Market): void {
-  const vTokenContract = VToken.bind(Address.fromBytes(market.id));
-  const comptrollerAddress = vTokenContract.comptroller();
   const comptrollerContract = Comptroller.bind(comptrollerAddress);
   const marketState = comptrollerContract.venusBorrowState(Address.fromBytes(market.id));
   market.xvsBorrowStateIndex = marketState.getIndex();
