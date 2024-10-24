be9542d329ba83f83d24181b2d60925d7300362f
Corey Rice <coreyarice@gmail.com>
2024-10-24 10:46:35 +0100

    feat: check market totals

--- subgraphs/venus/tests/scripts/checkAccountVTokens.ts
+++ subgraphs/venus/tests/scripts/checkAccountVTokens.ts
@@ -8,7 +8,49 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
 const subgraphClient = createSubgraphClient(process.env.SUBGRAPH_URL!);
 
+const checkMarketSupplyAndBorrowTotals = async () => {
+  const {
+    data: { markets },
+  } = await subgraphClient.getMarkets();
+  for (const market of markets) {
+    const vTokenContract = new ethers.Contract(market.id, VBep20Abi.abi, provider);
+    const totalSupply = await vTokenContract.totalSupply();
+    const totalBorrows = await vTokenContract.totalBorrows();
+    // Check total market supply
+    try {
+      assert.equal(
+        totalSupply.toString(),
+        market.totalSupplyVTokenMantissa,
+        `
+      incorrect total supply market ${market.symbol} ${
+          market.id
+        } contract ${totalSupply.toString()} subgraph ${market.totalSupplyVTokenMantissa.toString()}`,
+      );
+      console.log(`correct supply for ${market.symbol}`);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    // Check total market borrows
+    try {
+      assert.equal(
+        totalBorrows.toString(),
+        market.totalBorrowsMantissa.toString(),
+        `
+    incorrect total borrow on market ${market.symbol} ${
+          market.id
+        } contract ${totalBorrows.toString()} subgraph ${market.totalBorrowsMantissa.toString()}`,
+      );
+      console.log(`correct borrow for ${market.symbol}`);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+};
+
 const run = async () => {
+  await checkMarketSupplyAndBorrowTotals();
+
   let skip = 0;
   while (skip <= 20) {
     console.log(`processed ${skip * 25}...`);
