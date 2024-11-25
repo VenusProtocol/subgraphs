acad682e618407b6fb335ec7fd345e37d163574b
Corey Rice <coreyarice@gmail.com>
2024-11-25 15:22:54 -0300

    refactor: add converter address property

--- subgraphs/protocol-reserve/src/operations/create.ts
+++ subgraphs/protocol-reserve/src/operations/create.ts
@@ -14,6 +14,7 @@ export function createTokenConverter(tokenConverterAddress: Address): TokenConve
   const tokenConverterContract = TokenConverterContract.bind(tokenConverterAddress);
   const tokenConverter = new TokenConverter(getTokenConverterId(tokenConverterAddress));
   tokenConverter.destinationAddress = tokenConverterContract.destinationAddress();
+  tokenConverter.address = tokenConverterAddress;
 
   if (tokenConverterAddress.equals(riskFundConverterAddress)) {
     const riskFund = RiskFund.bind(riskFundAddress);
