8752bd0db8d2933a3873aa1402ecf46cdde476d5
Corey Rice <coreyarice@gmail.com>
2024-12-11 10:15:41 -0300

    ci: add cli authentication

--- subgraphs/isolated-pools/deploy.sh
+++ subgraphs/isolated-pools/deploy.sh
@@ -3,6 +3,7 @@
 version=($(jq -r '.version' package.json))
 
 if [[ $string == *"testnet"* ]]; then
+  yarn graph auth --studio $TESTNET_GRAPH_CLI_API_KEY
   yarn deploy:chapel --version-label $version
   yarn deploy:sepolia --version-label $version
   yarn deploy:arbitrumSepolia --version-label $version
@@ -10,6 +11,7 @@ if [[ $string == *"testnet"* ]]; then
   yarn deploy:optimismSepolia --version-label $version
   yarn deploy:baseSepolia --version-label $version
 else
+  yarn graph auth --studio $MAINNET_GRAPH_CLI_API_KEY
   yarn deploy:bsc --version-label $version
   yarn deploy:ethereum --version-label $version
   yarn deploy:arbitrum --version-label $version
--- subgraphs/protocol-reserve/deploy.sh
+++ subgraphs/protocol-reserve/deploy.sh
@@ -3,9 +3,11 @@
 version=($(jq -r '.version' package.json))
 
 if [[ $string == *"testnet"* ]]; then
+  yarn graph auth --studio $TESTNET_GRAPH_CLI_API_KEY 
   yarn deploy:chapel --version-label $version
   yarn deploy:sepolia --version-label $version
 else
+  yarn graph auth --studio $MAINNET_GRAPH_CLI_API_KEY
   yarn deploy:bsc --version-label $version
   yarn deploy:ethereum --version-label $version
 fi
