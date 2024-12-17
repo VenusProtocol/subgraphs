151aa7ad4532debd70052ea35d6a5ae58744207e
Corey Rice <coreyarice@gmail.com>
2024-12-17 14:44:06 -0300

    fix: relock in prepare script

--- subgraphs/cross-chain-governance/deploy.sh
+++ subgraphs/cross-chain-governance/deploy.sh
@@ -2,6 +2,8 @@
 
 version=($(jq -r '.version' package.json))
 
+yarn
+
 if [[ $version == *"testnet"* ]]; then
   yarn graph auth --studio $TESTNET_GRAPH_CLI_API_KEY
   yarn deploy:chapel --version-label $version
