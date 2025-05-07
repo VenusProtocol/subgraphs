01d7928d4465455a16236b79ff80aaa5c5c249d7
Gleiser Oliveira <gleiser.dev@gmail.com>
2025-05-07 11:03:23 -0600

    feat: deploy testnet governance to Alchemy

--- subgraphs/cross-chain-governance/deploy.sh
+++ subgraphs/cross-chain-governance/deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 version=($(jq -r '.version' package.json))
+alchemy_node=https://subgraphs.alchemy.com/api/subgraphs/deploy
+alchemy_ipfs=https://ipfs.satsuma.xyz
 
 yarn
 
 if [[ $version == *"testnet"* ]]; then
   yarn graph auth --studio $TESTNET_GRAPH_CLI_API_KEY
-  yarn deploy:chapel --version-label $version
-  yarn deploy:sepolia --version-label $version  
-  yarn deploy:arbitrumSepolia --version-label $version
-  yarn deploy:optimismSepolia --version-label $version
-  yarn deploy:zkSyncSepolia --version-label $version
-  yarn deploy:baseSepolia --version-label $version
+  yarn deploy:sepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
+  # obBNB is not supported in either Alchemy or The Graph
+  yarn deploy:arbitrumSepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
+  yarn deploy:zksyncSepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
+  yarn deploy:optimismSepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
+  yarn deploy:baseSepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
+  yarn deploy:unichainSepolia --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
 else
   yarn graph auth --studio $MAINNET_GRAPH_CLI_API_KEY
   yarn deploy:bsc --version-label $version
--- subgraphs/venus-governance/deploy.sh
+++ subgraphs/venus-governance/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 version=($(jq -r '.version' package.json))
+alchemy_node=https://subgraphs.alchemy.com/api/subgraphs/deploy
+alchemy_ipfs=https://ipfs.satsuma.xyz
 
 if [[ $version == *"testnet"* ]]; then
-  yarn graph auth --studio $TESTNET_GRAPH_CLI_API_KEY
-  yarn deploy:chapel --version-label $version
+  yarn deploy:chapel --node $alchemy_node --ipfs $alchemy_ipfs --version-label $version --deploy-key $TESTNET_GRAPH_CLI_ALCHEMY_KEY
 else
   yarn graph auth --studio $MAINNET_GRAPH_CLI_API_KEY
   yarn deploy:bsc --version-label $version
